3cda1e90f0d0d7351644e959499e81ccc3612e14
Felipe Souza <felipedb91@gmail.com>
2024-01-15 21:19:56 -0300

    test: ensure findUserByEmail has been called

--- src/use-case/create-user/createUsers.spec.ts
+++ src/use-case/create-user/createUsers.spec.ts
@@ -1,15 +1,28 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+import {
+  FindUserByEmailRepository,
+  User,
+} from '../../data/repository/FindUserByEmailRepository'
 import { CreateUserUseCase } from './createUseCase'
 
 const data = {
-  email: "sample@email.com",
-  name: "John doe",
-  password: "123456",
+  email: 'sample@email.com',
+  name: 'John doe',
+  password: '123456',
 }
+
+class UserRepository implements FindUserByEmailRepository {
+  findByEmail(email: string): Promise<User | undefined> {
+    return Promise.resolve(undefined)
+  }
+}
+
 describe('Create User use case Test', () => {
   test('should findbyEmail resposistory is called', async () => {
-    const sut = new CreateUserUseCase()
+    const repository = new UserRepository()
+    const sut = new CreateUserUseCase(repository)
+    const repositorySpy = vi.spyOn(repository, 'findByEmail')
     const result = await sut.execute(data)
-    expect(result).toBe(0)
+    expect(repository.findByEmail).toBeCalledTimes(1)
   })
 })
