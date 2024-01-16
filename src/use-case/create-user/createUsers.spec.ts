a066d7396c55b5e080069dd745f5e449cc0dd11f
Felipe Souza <felipedb91@gmail.com>
2024-01-15 21:34:34 -0300

    test: ensure throws if email already exists

--- src/use-case/create-user/createUsers.spec.ts
+++ src/use-case/create-user/createUsers.spec.ts
@@ -11,18 +11,41 @@ const data = {
   password: '123456',
 }
 
+const resultData = {
+  id: 'string',
+  name: 'string',
+  photoUrl: 'string',
+  password: 'string',
+  email: 'string',
+}
+
 class UserRepository implements FindUserByEmailRepository {
   findByEmail(email: string): Promise<User | undefined> {
     return Promise.resolve(undefined)
   }
 }
 
+const makeSut = () => {
+  const repository = new UserRepository()
+  const sut = new CreateUserUseCase(repository)
+  return {
+    repository,
+    sut,
+  }
+}
+
 describe('Create User use case Test', () => {
   test('should findbyEmail resposistory is called', async () => {
-    const repository = new UserRepository()
-    const sut = new CreateUserUseCase(repository)
-    const repositorySpy = vi.spyOn(repository, 'findByEmail')
-    const result = await sut.execute(data)
+    const { repository, sut } = makeSut()
+    vi.spyOn(repository, 'findByEmail')
+    await sut.execute(data)
     expect(repository.findByEmail).toBeCalledTimes(1)
+  })
+
+  test('should throw if emails already used', async () => {
+    const { repository, sut } = makeSut()
+    vi.spyOn(repository, 'findByEmail').mockResolvedValue(resultData)
+    const response = sut.execute(data)
+    await expect(response).rejects.toThrow('Email already used')
   })
 })
