404329bb41fc35578b386fa5f1980f298181abec
Felipe Souza <felipedb91@gmail.com>
2024-01-17 20:59:07 -0300

    test: should controller call use case

--- src/presentation/controller/login/signup/SignUpController.spec.ts
+++ src/presentation/controller/login/signup/SignUpController.spec.ts
@@ -1,8 +1,38 @@
-import test from 'node:test'
-import { describe, expect } from 'vitest'
+import { User } from '@/domain/model/user.model'
+import {
+  CreateUseData,
+  CreateUserUseCase,
+} from '@/domain/protocols/usecases/user/CreateUserUseCase'
+
+import { describe, expect, test, vi } from 'vitest'
+import { SignUpController } from './SignUpController'
+
+class CreateUserUseCaseStub implements CreateUserUseCase {
+  execute(data: CreateUseData): Promise<User> {
+    return Promise.resolve({
+      id: 'uuid',
+      email: 'valid@email.com',
+      name: 'Valid Name',
+      password: 'hasPassword',
+    })
+  }
+}
+
+const makeSut = () => {
+  const useCase = new CreateUserUseCaseStub()
+
+  const sut = new SignUpController(useCase)
+  return {
+    sut,
+    useCase,
+  }
+}
 
 describe('SignUpController', async () => {
-  test('should controller call use case', () => {
-    expect(0).toBe(0)
+  test('should controller call use case', async () => {
+    const { sut, useCase } = makeSut()
+    vi.spyOn(useCase, 'execute')
+    await sut.run()
+    expect(useCase.execute).toHaveBeenCalledTimes(1)
   })
 })
