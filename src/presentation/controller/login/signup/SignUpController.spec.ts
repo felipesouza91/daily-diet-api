fdd59505ad6bd1c920897c89a6ee4067e85dbe10
Felipe Souza <felipedb91@gmail.com>
2024-01-17 21:17:59 -0300

    test: ensure useCase has been called with correct value

--- src/presentation/controller/login/signup/SignUpController.spec.ts
+++ src/presentation/controller/login/signup/SignUpController.spec.ts
@@ -18,6 +18,12 @@ class CreateUserUseCaseStub implements CreateUserUseCase {
   }
 }
 
+const fakeInputRequest = {
+  name: 'some name',
+  email: 'valid@email.com',
+  password: 'valid@password.com',
+}
+
 const makeSut = () => {
   const useCase = new CreateUserUseCaseStub()
 
@@ -32,7 +38,14 @@ describe('SignUpController', async () => {
   test('should controller call use case', async () => {
     const { sut, useCase } = makeSut()
     vi.spyOn(useCase, 'execute')
-    await sut.run()
+    await sut.handler(fakeInputRequest)
     expect(useCase.execute).toHaveBeenCalledTimes(1)
+  })
+
+  test('should useCase has been called with correct values', async () => {
+    const { sut, useCase } = makeSut()
+    vi.spyOn(useCase, 'execute')
+    await sut.handler(fakeInputRequest)
+    expect(useCase.execute).toHaveBeenCalledWith(fakeInputRequest)
   })
 })
